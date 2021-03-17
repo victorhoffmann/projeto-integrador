2ccc5f42a07bd773df81f4c5bf77ed0e6a7ed274
Pehoffmann <pedrob_hoffmann@outlook.com>
2021-03-16 22:08:48 -0300

    bugfix/02-className

--- src/components/MainContato/index.js
+++ src/components/MainContato/index.js
@@ -1,61 +1,54 @@
-import './style.css'
+import "./style.css";
 
 const MainContato = () => {
-    return (
-        <>
-            <div class="container">
-                <div class="text">
-                    Contato</div>
-                <form action="#">
-                    <div class="form-row names">
-                        <div class="input-data">
-                            <input type="text" required></input>
-                            <div class="underline">
-                            </div>
-                            <label for="name">Primeiro nome.</label>
-                        </div>
-                        <div class="input-data">
-                            <input type="text" required></input>
-                            <div class="underline">
-                            </div>
-                            <label for="">Ultimo nome.</label>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="input-data">
-                            <input type="text" required></input>
-                            <div class="underline">
-                            </div>
-                            <label for="email">Endereço de E-mail.</label>
-                        </div>
-                        <div class="input-data">
-                            <input type="text" required></input>
-                            <div class="underline">
-                            </div>
-                            <label for="tel">Número de telefone. </label>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="input-data textarea">
-                            <textarea rows="8" cols="80" required></textarea>
-                            <br />
-                            <div class="underline">
-                            </div>
-                            <label for="message">Escreva sua mensagem!</label>
-                            <br />
-                            <div class="form-row submit-btn">
-                                <div class="input-data">
-                                    <div class="inner"></div>
-                                    <input type="submit" value="submit"></input>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-
+  return (
+    <>
+      <div className="container">
+        <div className="text">Contato</div>
+        <form action="#">
+          <div className="form-row names">
+            <div className="input-data">
+              <input type="text" required></input>
+              <div className="underline"></div>
+              <label for="name">Primeiro nome.</label>
             </div>
-        </>
-    )
-}
+            <div className="input-data">
+              <input type="text" required></input>
+              <div className="underline"></div>
+              <label for="">Ultimo nome.</label>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" required></input>
+              <div className="underline"></div>
+              <label for="email">Endereço de E-mail.</label>
+            </div>
+            <div className="input-data">
+              <input type="text" required></input>
+              <div className="underline"></div>
+              <label for="tel">Número de telefone. </label>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="input-data textarea">
+              <textarea rows="8" cols="80" required></textarea>
+              <br />
+              <div className="underline"></div>
+              <label for="message">Escreva sua mensagem!</label>
+              <br />
+              <div className="form-row submit-btn">
+                <div className="input-data">
+                  <div className="inner"></div>
+                  <input type="submit" value="submit"></input>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default MainContato
+export default MainContato;
--- src/components/MainMinhaConta/index.js
+++ src/components/MainMinhaConta/index.js
@@ -7,33 +7,33 @@ const MainMinhaConta = () => {
       <h4>Você precisa ser cadastrado ou já ter um cadastro para continuar.</h4>
       <section className="formularios">
         <div className="signIn">
-          <div class="headerForm">
+          <div className="headerForm">
             <p>Já é cadastrado?</p>
             Então entre com seus dados de login e senha.
           </div>
-          <div class="bodyForm">
+          <div className="bodyForm">
             E-mail e Senha são obrigatórios para login.
             <h3>Dados Pessoais</h3>
             <form>
-              <div class="form-row">
-                <div class="form-group col-md-9">
+              <div className="form-row">
+                <div className="form-group col-md-9">
                   <label for="inputEmail">E-MAIL, CPF ou CNPJ:</label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="inputEmail"
                   ></input>
                 </div>
-                <div class="form-group col-md-6">
+                <div className="form-group col-md-6">
                   <label for="inputPassword">SUA SENHA:</label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="inputPassword"
                   ></input>
                 </div>
               </div>
-              <div class="esqueci">
+              <div className="esqueci">
                 <a
                   href="#"
                   onclick="window.open('./Reenvio/esqueciLogin.html', 'REENVIO_LOGIN','top=50,left=50,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=475,height=400'); return false"
@@ -41,7 +41,7 @@ const MainMinhaConta = () => {
                   ESQUECI MEU LOGIN
                 </a>
               </div>
-              <div class="esqueci">
+              <div className="esqueci">
                 <a
                   href="#"
                   onclick="window.open('./Reenvio/esqueciSenha.html', 'REENVIO_SENHA','top=50,left=50,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=475,height=400'); return false"
@@ -49,74 +49,74 @@ const MainMinhaConta = () => {
                   ESQUECI MINHA SENHA
                 </a>
               </div>
-              <button type="submit" class="btn btn-entrar btn-success">
+              <button type="submit" className="btn btn-entrar btn-success">
                 ENTRAR
               </button>
               Entrar com a sua conta do Facebook ou Google:
               <div>
-                <button type="submit" class="btn btn-entrarCom btn-primary">
+                <button type="submit" className="btn btn-entrarCom btn-primary">
                   Entrar com Facebook
                 </button>
-                <button type="submit" class="btn btn-entrarCom btn-danger">
+                <button type="submit" className="btn btn-entrarCom btn-danger">
                   Entrar com Google
                 </button>
               </div>
             </form>
           </div>
         </div>
-        <div class="signUp">
-          <div class="headerForm">
+        <div className="signUp">
+          <div className="headerForm">
             <p>Ainda não é cadastrado no site?</p>
             Faça agora mesmo o seu cadastro, é rápido!
           </div>
-          <div class="bodyForm">
+          <div className="bodyForm">
             Todos os campos são de preenchimento obrigatório.
             <h3>Dados Cadastrais</h3>
             <form>
-              <div class="form-row">
-                <div class="form-group col-md-9">
+              <div className="form-row">
+                <div className="form-group col-md-9">
                   <label for="inputEmail2">E-MAIL:</label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="inputEmail2"
                   ></input>
                 </div>
-                <div class="form-group col-md-6">
+                <div className="form-group col-md-6">
                   <label for="inputPassword2">CRIE SUA SENHA:</label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="inputPassword2"
                   ></input>
                 </div>
-                <div class="form-group col-md-6">
+                <div className="form-group col-md-6">
                   <label for="inputConfirmPassword">CONFIRME SUA SENHA:</label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="inputConfirmPassword"
                   ></input>
                 </div>
-                <div class="form-group col-md-9">
+                <div className="form-group col-md-9">
                   <label for="inputCpfOuCnpj">
                     CPF ou CNPJ (Apenas números):
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="inputCpfOuCnpj"
                   ></input>
                 </div>
-                <div class=" form-group col-md-6">
+                <div className=" form-group col-md-6">
                   <label for="inputCep">CEP:</label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="inputCep"
                   ></input>
                 </div>
-                <button type="submit" class="btn btn-cadastrar btn-success">
+                <button type="submit" className="btn btn-cadastrar btn-success">
                   CADASTRAR
                 </button>
               </div>
