2924d9683b509a043c7e8b4c7d6a760bb54e3ddf
viictorhoffmann <viictorhoffmann@gmail.com>
2021-04-01 19:50:42 -0300

    feature: criando painel adm

---
+++ frontend/src/components/MenuAdm/index.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+const MenuAdm = () => {
+    return (
+        <div className="menuAdm">
+            <h3>Periféricos.Net</h3>
+            <ul className='listMenuAdm'>
+                <a href="#"><li>Inicio</li></a>
+                <a href='#'><li>Produtos</li></a>
+                <a href='#'><li>Teste</li></a>
+                <a href='#'><li>Teste</li></a>
+                <a href='#'><li>Teste</li></a>
+                <a href='#'><li>Teste</li></a>
+                <a href='#'><li>Teste</li></a>
+                <a href='#'><li>Teste</li></a>
+            </ul>
+        </div>
+    )
+}
+
+export default MenuAdm
