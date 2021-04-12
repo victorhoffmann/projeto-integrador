e2f2f0f830eb37082d01c898429df4798f9f9bb3
viictorhoffmann <viictorhoffmann@gmail.com>
2021-04-12 19:10:22 -0300

    bugfix: corrigindo espacamentos no esqueci-senha

--- frontend/src/components/Senha/index.js
+++ frontend/src/components/Senha/index.js
@@ -1,39 +1,43 @@
 const Senha = () => {
     return (
-        <section className='formularios'>    
-            <div className="signUp">
-            <div className="headerForm">
-                <p>Apenas informe no campo a seguir o endereço de e-mail cadastrado e reenviaremos sua senha para o e-mail informado.</p>
-            </div>
-            <div className="bodyForm">
-                <form>
-                <div className="form-row">
-                    <div className="form-group col-md-9">
-                    <label for="inputEmail2">E-MAIL:</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="inputEmail2"
-                    ></input>
+        <>
+            <section className='formularios'>
+                <div className="signUp">
+                    <div className="headerForm">
+                        <p>Apenas informe no campo a seguir o endereço de e-mail cadastrado e reenviaremos sua senha para o e-mail informado.</p>
                     </div>
+                    <div className="bodyForm">
+                        <form>
+                            <div className="form-row">
+                                <div className="form-group col-md-9">
+                                    <label for="inputEmail2">E-MAIL:</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="inputEmail2"
+                                    ></input>
+                                </div>
 
-                    <div className="form-row submit-btn__entrar form-btns">
-                    <div className="input-data btn-voltar">
-                      <div className="inner"></div>
-                      <a href="./login">
-                      <input type="button" value="voltar"></input></a>
-                    </div>
-                    <div className="input-data btn-entrar">
-                      <div className="inner"></div>
-                      <input type="submit" value="entrar"></input>
-                    </div>
-                    </div>
+                                <div className="form-row submit-btn__entrar form-btns">
+                                    <div className="input-data btn-voltar">
+                                        <div className="inner"></div>
+                                        <a href="./login">
+                                            <input type="button" value="voltar"></input></a>
+                                    </div>
+                                    <div className="input-data btn-entrar">
+                                        <div className="inner"></div>
+                                        <input type="submit" value="entrar"></input>
+                                    </div>
+                                </div>
 
+
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                </form>
-            </div>
-            </div>
-        </section>
+            </section>
+            <div className="espacoFinalForm"></div>
+        </>
     )
 }
 
